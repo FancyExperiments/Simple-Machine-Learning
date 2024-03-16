16f24f2951543ce0819a56db04bbecb54103e638
Fancy <163139305+FancyExperiments@users.noreply.github.com>
2024-03-16 14:36:47 +0100

    Create Simple Word2Vec.py

---
+++ Simple Word2Vec.py
@@ -0,0 +1,19 @@
+# Wikipedia Models:
+# https://wikipedia2vec.github.io/wikipedia2vec/pretrained/
+
+from gensim.models import KeyedVectors
+import pickle
+
+# Laden des vortrainierten word2vec-Modells im .pkl-Format
+with open('path/to/word2vec/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Zwei Wörter, deren Ähnlichkeit wir überprüfen möchten
+wort1 = 'Auto'
+wort2 = 'Fahrzeug'
+
+# Berechnung der Ähnlichkeit zwischen den beiden Wörtern
+aehnlichkeit = model.similarity(wort1, wort2)
+
+print(f'Die Ähnlichkeit zwischen "{wort1}" und "{wort2}" beträgt: {aehnlichkeit}')
+
