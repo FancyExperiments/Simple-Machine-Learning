59d97f1c4e5151e29a60f065e19cb2ae6d0aaf8a
Fancy <163139305+FancyExperiments@users.noreply.github.com>
2024-03-16 14:38:07 +0100

    Update Simple Word2Vec.py

--- Simple Word2Vec.py
+++ Simple Word2Vec.py
@@ -5,7 +5,7 @@ from gensim.models import KeyedVectors
 import pickle
 
 # Laden des vortrainierten word2vec-Modells im .pkl-Format
-with open('path/to/word2vec/model.pkl', 'rb') as file:
+with open('path/to/word2vec/dewiki_20180420_100d.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Zwei Wörter, deren Ähnlichkeit wir überprüfen möchten
